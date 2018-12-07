c65c61558c9a702ba877b359f6ccf33f2e3551cb
HAKETA96 <44375787+HAKETA96@users.noreply.github.com>
2018-12-07 17:34:59 +0000

    resposive header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $('.nav li a').on('mouseover', function(){
   console.log(1)
-  $(this).css('color', 'black')
-})
+  $(this).css('color', 'yellow')
+/*  $(this).css('color', '#39CBE0') */
+  $('.nav li a').on('mouseout', function() {
+    $(this).css('color','white')
+});
+});
