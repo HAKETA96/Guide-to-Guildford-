474d160c51a82122aceb210738d6da989f591e08
HAKETA96 <44375787+HAKETA96@users.noreply.github.com>
2018-12-07 23:16:38 +0000

    remove file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-$('.nav li a').on('mouseover', function(){
+/* Colour change of headings in header on hover */
+$('.nav li a').on('mouseover', function() {
   console.log(1)
-  $(this).css('color', 'yellow')
-/*  $(this).css('color', '#39CBE0') */
+  $(this).css('color','yellow')
   $('.nav li a').on('mouseout', function() {
+    $(this).css('color','white')
+});
+});
+
+/* Colour change of explore guildford on hover*/
+$('.fullscreen h1').on('mouseover', function(){
+  console.log(1)
+  $(this).css('color','yellow')
+  $('h1').on('mouseout', function() {
     $(this).css('color','white')
 });
 });
