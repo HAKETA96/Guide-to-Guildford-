44e35088828f28917237be33354867a6ee19fb62
HAKETA96 <44375787+HAKETA96@users.noreply.github.com>
2018-12-12 21:54:02 +0000

    eating out updated
    flip 3d design removed and replaced with cards
    
    new images of restaurants added

--- js/about.js
+++ js/about.js
@@ -1,20 +1,18 @@
 /* Colour change of headings in header on hover */
-
 $(".nav li a").on("mouseover", function() {
   console.log(1)
   $(this).css("color","yellow")
   $(".nav li a").on("mouseout", function() {
-    $(this).css("color","white")
+    $(this).css("color","#DCAE1D")
 });
 });
 
 /* Colour change of explore guildford on hover */
-
 $(".fullscreen h1").on("mouseover", function(){
   console.log(1)
   $(this).css("color","yellow")
   $(".fullscreen h1").on("mouseout", function() {
-    $(this).css("color","white")
+    $(this).css("color","#DCAE1D")
 });
 });
 
@@ -47,11 +45,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-/*search box*/
-var $searchSection = $('<div class="student-search"></div>');
-$searchSection.append('<input id = "search-criteria" placeholder="Search for students..."></input>');
-$searchSection.append('<button>Search</button>');
-
-//Append search section
-$('.page-header').append($searchSection);
